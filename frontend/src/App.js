e6fbcb685e8d6fbbd7a24e13ea79f0061ae3faf7
Tim Holmes <timholmez@gmail.com>
2020-05-21 22:00:14 -0400

    Front end pulling data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
+import { Line } from 'react-chartjs-2';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    pollList: []
+  }
+
+  componentDidMount() {
+    return this.getDataFromApi()
+  }
+
+  async getDataFromApi() {
+    const response = await axios.get('http://localhost:5000/api/v1/resources/congress-outlook')
+    this.setState((state, props) => {
+      return {
+        pollList: response.data.poll_list
+      }
+    })
+  }
+
+  render() {
+    const data = {
+      labels: this.state.pollList.map((e) => e.createddate),
+      datasets: [{
+        label: 'Adjusted Dem',
+        data: this.state.pollList.map((e) => e.adjusted_dem),
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        borderColor: '#0377fc',
+        borderWidth: 1
+      }, {
+        label: 'Adjusted Rep',
+        data: this.state.pollList.map((e) => e.adjusted_rep),
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        borderColor: '#fc0303',
+        borderWidth: 1
+      }]
+    }
+
+    return (
+      <div className="app">
+        <header className="header">
+          <h1>Capitol Tracker</h1>
+          <h6 className="sub-heading">Data driven political decisions</h6>
+        </header>
+
+        <Line 
+          className="line-graph"
+          data={data} />
+      </div>
+    );
+  }
 }
 
 export default App;
